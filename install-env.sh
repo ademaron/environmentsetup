4b600f808bf9df3e882fe4c85fcacd075862674e
ademaron <ademaron36@gmail.com>
2015-10-23 23:46:08 -0500

    added php5

--- install-env.sh
+++ install-env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-install apt-get install apache2 git
+sudo apt-get update -y
+sudo apt-get install -y apache2 git php5 mysql-client
+curl -sS https://getcomposer.org/installer | php
+sudo php composer.phar require aws/aws-sdk-php
+git clone https://github.com/ademaron/environmentsetup.git
 echo "Go scarlet hawks" > /tmp/hello.txt
 cp index.html /var/www/html
 
