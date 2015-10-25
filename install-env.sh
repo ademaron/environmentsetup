8f7a7e22ea31ce5bca4a79f04dcb4a36b0f3267a
ademaron <ademaron36@gmail.com>
2015-10-25 17:57:43 -0500

    environment setup script = installwebserver.sh

--- install-env.sh
+++ install-env.sh
@@ -3,7 +3,10 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 mysql-client
 curl -sS https://getcomposer.org/installer | php
 sudo php composer.phar require aws/aws-sdk-php
-git clone https://github.com/ademaron/environmentsetup.git
-echo "Go scarlet hawks" > /tmp/hello.txt
-cp index.html /var/www/html
-
+cp -R vendor/ /var/www/html
+git clone https://github.com/ademaron/applicationsetup.git
+cp -R applicationsetup/* /var/www/html/
+php /var/www/setup.php
+chmod 600 /var/www/html/setup.php
+mkdir /var/www/html/uploads
+chmod 777 /var/www/html/uploads
